4c095c9e808d09d4d8f1124a87825cca62646930
WHY <757184917@qq.com>
2023-11-04 02:07:03 +0800

    modified:   src/pages/WebTerminal.vue 	new file:   src/pages/WebTerminal/TerminalComponent.vue 	deleted:    src/pages/WebTerminal/WebTerminal.vue

--- src/pages/WebTerminal.vue
+++ src/pages/WebTerminal.vue
@@ -6,7 +6,15 @@
         <!-- 主页面内容 -->
         <div class="container-fluid mx-auto">
             <div class="col-12 col-xl-10 mx-auto bg-white border-radius-sm box-shadow-console pt-1 mt-4 box-shadow-settings">
-                <Terminal></Terminal>
+                <div class="terminals">
+                    <div class="titles">
+                        <div class="terminal-title right-split"><span class="title-name">终端 1</span><span class="material-icons close-btn">cancel</span></div>
+                        <div class="terminal-title "><span class="title-name">终端 2</span><span class="material-icons close-btn">cancel</span></div>
+                        <div class="terminal-title active"><span class="title-name">终端 3</span><span class="material-icons close-btn">cancel</span></div>
+                        <div class="add-terminal"><span class="material-icons add-btn">add</span></div>
+                    </div>
+                    <Terminal class="terminal-board"></Terminal>
+                </div>
             </div>
         </div>
         <!-- 主页面内容结束 -->
@@ -15,8 +23,7 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
-import Terminal from "./WebTerminal/WebTerminal.vue"
-
+import Terminal from "./WebTerminal/TerminalComponent.vue";
 
 // 注册组件
 export default {
@@ -31,7 +38,7 @@ export default {
             Title: {
                 tinyTitle: "终端",
                 fullTitle: "Web终端"
-            },
+            }
         };
     },
     methods: {
@@ -40,15 +47,109 @@ export default {
             this.$emit("ToggleNavSideBar");
         }
     },
-    mounted() {
-
-    },
-    beforeDestroy() {
-
-    }
+    mounted() {},
+    beforeDestroy() {}
 };
 </script>
 
 <style scoped>
+.terminals {
+    padding-top: 2px;
+    padding-left: 7px;
+    padding-right: 7px;
+}
 
+.terminals .titles {
+    height: 40px;
+    margin-bottom: 5px;
+    background-color: rgb(240, 240, 240);
+    display: flex;
+    overflow-x: auto;
+    overflow-y: hidden;
+}
+.terminals .titles::-webkit-scrollbar {
+    height: 5px;
+}
+.terminals .titles::-webkit-scrollbar-thumb {
+    background-color: rgb(170, 170, 170);
+    border-radius: 6px;
+}
+.terminals .titles::-webkit-scrollbar-thumb:hover {
+    background-color: rgb(130, 130, 130);
+}
+.terminals .titles::-webkit-scrollbar-thumb:active {
+    background-color: rgb(110, 110, 110);
+}
+.terminals .titles::-webkit-scrollbar-track {
+    background-color: white;
+}
+
+.terminals .titles .terminal-title {
+    max-width: 200px;
+    min-width: 150px;
+    height: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    cursor: default;
+    position: relative;
+}
+.terminals .titles .terminal-title:hover {
+    background-color: rgb(245,245,245);
+}
+.terminals .titles .terminal-title.active {
+    background-color: rgb(255, 255, 255);
+    color: rgb(105, 192, 255);
+}
+.terminals .titles .terminal-title.right-split::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translate(0, -50%);
+    height: 20px;
+    width: 1px; /* 线的宽度 */
+    background-color: rgb(114, 114, 114); /* 线的颜色 */
+}
+
+.terminals .titles .terminal-title .title-name {
+    font-size: 14px;
+    flex-grow: 1;
+}
+.terminals .titles .terminal-title .close-btn {
+    display: block;
+    opacity: 0.3;
+    font-size: 15px;
+    user-select: none;
+    text-align: right;
+    width: 15px;
+    color: #7b809a;
+}
+.terminals .titles .terminal-title .close-btn:hover {
+    opacity: 0.8;
+}
+
+.terminals .titles .add-terminal {
+    min-width: 25px;
+    padding-left: 5px;
+    height: 100%;
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+    cursor: default;
+}
+.terminals .titles .add-terminal .add-btn {
+    user-select: none;
+    color: #7b809a;
+}
+.terminals .titles .add-terminal .add-btn:hover {
+    color: rgb(80, 80, 80);
+}
+
+.terminals .terminal-board {
+    height: 600px;
+}
 </style>
---
+++ src/pages/WebTerminal/TerminalComponent.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <div ref="terminal" class="terminal">
+        </div>
+    </div>
+</template>
+
+<script>
+import "xterm/css/xterm.css";
+import axios from "axios";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import { AttachAddon } from "xterm-addon-attach";
+import { CanvasAddon } from "xterm-addon-canvas";
+
+export default {
+    name: "WebTerminal",
+    data() {
+        return {
+            ip: "127.0.0.1",
+            port: 3000,
+            terminal: null,
+            fitAddon: null,
+            socketURL: "",
+            socket: null,
+            pid: null
+        };
+    },
+    methods: {
+        initTerminal() {
+            this.terminal = new Terminal({
+                fontSize: 17,
+                cursorStyle: "bar",
+                cursorBlink: true,
+                fontFamily: 'Monaco, Consolas, "Courier New", monospace'
+            });
+            let terminalDom = this.$refs.terminal;
+            this.terminal.open(terminalDom);
+            this.fitAddon = new FitAddon();
+            this.terminal.loadAddon(this.fitAddon);
+            this.terminal.loadAddon(new CanvasAddon());
+            this.fitAddon.fit();
+        },
+        initSocket() {
+            axios
+                .post(`http://${this.ip}:${this.port}/create-terminal`, {
+                    cols: this.terminal.cols,
+                    rows: this.terminal.rows
+                })
+                .then((res) => {
+                    let processId = res.data;
+                    console.log("get processId:", processId);
+                    this.pid = processId;
+                    this.socketURL = `ws://${this.ip}:${this.port}/terminals/${this.pid}`;
+                    console.log("open webSocket URL:", this.socketURL);
+                    this.socket = new WebSocket(this.socketURL);
+                    this.terminal.loadAddon(new AttachAddon(this.socket));
+                });
+        },
+        resizeTerminal() {
+            if (!this.pid || this.terminal.rows < 3) {
+                return;
+            }
+            axios.post(`http://${this.ip}:${this.port}/terminal-resize`, {
+                cols: this.terminal.cols,
+                rows: this.terminal.rows,
+                pid: this.pid
+            });
+        }
+    },
+    mounted() {
+        this.initTerminal();
+        this.initSocket();
+        let resizeTimeout = null;
+        new ResizeObserver(() => {
+            if(resizeTimeout)
+                clearTimeout(resizeTimeout);
+            resizeTimeout = setTimeout(() => {
+                this.fitAddon.fit();
+                this.resizeTerminal();
+            }, 50);
+        }).observe(this.$refs.terminal);
+    },
+    beforeDestroy() {
+        this.socket.close();
+        this.terminal.dispose();
+    }
+};
+</script>
+
+<style scoped>
+.terminal {
+    width: 100%;
+    height: 100%;
+}
+.terminal ::-webkit-scrollbar {
+    width: 5px;
+}
+.terminal ::-webkit-scrollbar-thumb {
+    background-color: rgb(170, 170, 170);
+    border-radius: 5px;
+}
+.terminal ::-webkit-scrollbar-thumb:hover {
+    background-color: rgb(130, 130, 130);
+}
+.terminal ::-webkit-scrollbar-thumb:active {
+    background-color: rgb(110, 110, 110);
+}
+.terminal ::-webkit-scrollbar-track {
+    background-color: white;
+}
+</style>
